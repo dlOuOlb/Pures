0b6fde9f36a28075f4a380f8de9dffe053232f51
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2019-07-08 16:10:26 +0900

    Add files via upload

---
+++ Sources/strpain.c
@@ -0,0 +1,176 @@
+﻿#ifdef _INC_STRPURE
+#ifdef _SRC_STRPAIN
+
+static size_t Func_(TT,Length,XXXX)(TT_XXXX *const String)
+{
+	return _StrP_Length_(String->TT,OxXXXX);
+}
+static size_t Func_(TT,Find,XXXX)(TT_XXXX *const String,TT_UNIT Character,const _Bool Mode)
+{
+	TT_UNIT *const Where=(Mode)?(_StrP_Find_Forward_(String->TT,Character)):(_StrP_Find_Reverse_(String->TT,Character));
+
+	return ((Where)?(Where-(String->TT)):(OxXXXX));
+}
+static size_t Func_(TT,Search,XXXX)(TT_XXXX *const String,TT_XXXX *const SubString)
+{
+	TT_UNIT *const Where=_StrP_Search_(String->TT,SubString->TT);
+
+	return ((Where)?(Where-(String->TT)):(OxXXXX));
+}
+static int Func_(TT,Reset,XXXX)(tt_xxxx *const restrict String)
+{
+	return memset_s(String,sizeof(tt_xxxx),0,sizeof(tt_xxxx));
+}
+
+static int Func_(TT,Copy,XXXX)(tt_xxxx *const restrict Target,TT_XXXX *const restrict Source)
+{
+	return _StrP_Copy_(Target->TT,OxXXXX,Source->TT);
+}
+static int Func_(TT,Conc,XXXX)(tt_xxxx *const restrict Target,TT_XXXX *const restrict Source)
+{
+	return _StrP_Conc_(Target->TT,OxXXXX,Source->TT);
+}
+static int Func_(TT,Crop,XXXX)(tt_xxxx *const Target,TT_XXXX *const Source,const size_t Offset,const size_t Length)
+{
+	const size_t Limit=_StrP_Length_(Source->TT,OxXXXX);
+
+	if(Limit<OxXXXX)
+	{
+		const size_t There=Offset+Length;
+
+		if(Offset>There);
+		else if(There>Limit);
+		else
+			return _StrP_Cast_(Target->TT,OxXXXX,Source->TT+Offset,Length);
+	}
+	else;
+
+	if(Target)
+		Target->TT[0]=0;
+	
+	return ERANGE;
+}
+static int Func_(TT,Comp,XXXX)(TT_XXXX *const Left,TT_XXXX *const Right)
+{
+	return _StrP_Comp_(Left->TT,Right->TT,OxXXXX);
+}
+static int Func_(TT,Cast,XXXX)(void *const restrict Target,const void *const restrict Source)
+{
+	const size_t Limit=OxXXXX-0x0001;
+
+	return _StrP_Cast_(Target,OxXXXX,Source,Limit);
+}
+
+#else
+
+#if(1)
+#define _Conc_(A,B) A##B
+#define Conc_(A,B) _Conc_(A,B)
+
+#define _Func_(Type,Name,Nums) StrP_##Type##_##Name##_0x##Nums##_
+#define Func_(Type,Name,Nums) _Func_(Type,Name,Nums)
+
+#define OxXXXX Conc_(0x,XXXX)
+
+#define tt_unit Conc_(strp_,tt)
+#define TT_UNIT Conc_(STRP_,TT)
+
+#define tt_xxxx Conc_(tt_unit,Conc_(_0x,XXXX))
+#define TT_XXXX Conc_(TT_UNIT,Conc_(_0X,XXXX))
+#endif
+
+#define _SRC_STRPAIN
+
+#if(1)
+#define tt nc
+#define TT NC
+#define _StrP_Length_ strnlen_s
+#define _StrP_Find_Forward_ strchr
+#define _StrP_Find_Reverse_ strrchr
+#define _StrP_Search_ strstr
+#define _StrP_Copy_ strcpy_s
+#define _StrP_Conc_ strcat_s
+#define _StrP_Comp_ strncmp
+#define _StrP_Cast_ strncpy_s
+
+#define XXXX 0010
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0040
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0100
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0400
+#include "strpain.c"
+#undef XXXX
+
+#undef _StrP_Cast_
+#undef _StrP_Comp_
+#undef _StrP_Conc_
+#undef _StrP_Copy_
+#undef _StrP_Search_
+#undef _StrP_Find_Reverse_
+#undef _StrP_Find_Forward_
+#undef _StrP_Length_
+#undef TT
+#undef tt
+#endif
+
+#if(1)
+#define tt wc
+#define TT WC
+#define _StrP_Length_ wcsnlen_s
+#define _StrP_Find_Forward_ wcschr
+#define _StrP_Find_Reverse_ wcsrchr
+#define _StrP_Search_ wcsstr
+#define _StrP_Copy_ wcscpy_s
+#define _StrP_Conc_ wcscat_s
+#define _StrP_Comp_ wcsncmp
+#define _StrP_Cast_ wcsncpy_s
+
+#define XXXX 0010
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0040
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0100
+#include "strpain.c"
+#undef XXXX
+#define XXXX 0400
+#include "strpain.c"
+#undef XXXX
+
+#undef _StrP_Cast_
+#undef _StrP_Comp_
+#undef _StrP_Conc_
+#undef _StrP_Copy_
+#undef _StrP_Search_
+#undef _StrP_Find_Reverse_
+#undef _StrP_Find_Forward_
+#undef _StrP_Length_
+#undef TT
+#undef tt
+#endif
+
+#undef _SRC_STRPAIN
+
+#if(1)
+#undef TT_XXXX
+#undef tt_xxxx
+#undef TT_UNIT
+#undef tt_unit
+#undef OxXXXX
+#undef Func_
+#undef _Func_
+#undef Conc_
+#undef _Conc_
+#endif
+
+#endif
+
+#else
+static void _StrP_Void_(void) { (void)(_StrP_Void_); }
+#endif
---
+++ Sources/strpure.c
@@ -0,0 +1,261 @@
+﻿#ifdef __STDC__
+
+#if(__STDC__)
+
+#ifdef __STDC_VERSION__
+
+#if(__STDC_VERSION__<201710L)
+#error The compiler does not satisfy the C18 Standard.
+#else
+
+#ifdef __STDC_LIB_EXT1__
+
+#if(1)
+#define __STDC_WANT_LIB_EXT1__ (1)
+#include <errno.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include "strpure.h"
+#endif
+
+#if(1)
+typedef char strp_nc;
+typedef const char STRP_NC;
+
+typedef wchar_t strp_wc;
+typedef const wchar_t STRP_WC;
+
+_Static_assert(CHAR_BIT==8,"CHAR_BIT != 8");
+_Static_assert(sizeof(char)==1,"sizeof(char) != 1");
+_Static_assert(sizeof(errno_t)==sizeof(int),"sizeof(errno_t) != sizeof(int)");
+_Static_assert(sizeof(rsize_t)==sizeof(size_t),"sizeof(rsize_t) != sizeof(size_t)");
+_Static_assert(RSIZE_MAX>(0x0400*sizeof(wchar_t)),"RSIZE_MAX <= (0x0400*sizeof(wchar_t))");
+
+#include "strpain.c"
+#endif
+
+#if(1)
+static const char _StringVersion[16]="2019.07.08";
+#endif
+
+#if(1)
+STRPACK StrP=
+{
+	.NC=
+	{
+		.Length=
+		{
+			.x0010_=StrP_NC_Length_0x0010_,
+			.x0040_=StrP_NC_Length_0x0040_,
+			.x0100_=StrP_NC_Length_0x0100_,
+			.x0400_=StrP_NC_Length_0x0400_
+		},
+		.Find=
+		{
+			.Char=
+			{
+				.x0010_=StrP_NC_Find_0x0010_,
+				.x0040_=StrP_NC_Find_0x0040_,
+				.x0100_=StrP_NC_Find_0x0100_,
+				.x0400_=StrP_NC_Find_0x0400_
+			},
+			.String=
+			{
+				.x0010_=StrP_NC_Search_0x0010_,
+				.x0040_=StrP_NC_Search_0x0040_,
+				.x0100_=StrP_NC_Search_0x0100_,
+				.x0400_=StrP_NC_Search_0x0400_
+			}
+		},
+		.Reset=
+		{
+			.x0010_=StrP_NC_Reset_0x0010_,
+			.x0040_=StrP_NC_Reset_0x0040_,
+			.x0100_=StrP_NC_Reset_0x0100_,
+			.x0400_=StrP_NC_Reset_0x0400_
+		},
+		.Copy=
+		{
+			.x0010_=StrP_NC_Copy_0x0010_,
+			.x0040_=StrP_NC_Copy_0x0040_,
+			.x0100_=StrP_NC_Copy_0x0100_,
+			.x0400_=StrP_NC_Copy_0x0400_
+		},
+		.Conc=
+		{
+			.x0010_=StrP_NC_Conc_0x0010_,
+			.x0040_=StrP_NC_Conc_0x0040_,
+			.x0100_=StrP_NC_Conc_0x0100_,
+			.x0400_=StrP_NC_Conc_0x0400_
+		},
+		.Crop=
+		{
+			.x0010_=StrP_NC_Crop_0x0010_,
+			.x0040_=StrP_NC_Crop_0x0040_,
+			.x0100_=StrP_NC_Crop_0x0100_,
+			.x0400_=StrP_NC_Crop_0x0400_
+		},
+		.Comp=
+		{
+			.x0010_=StrP_NC_Comp_0x0010_,
+			.x0040_=StrP_NC_Comp_0x0040_,
+			.x0100_=StrP_NC_Comp_0x0100_,
+			.x0400_=StrP_NC_Comp_0x0400_
+		},
+		.Cast=
+		{
+			.x0010=
+			{
+				.x0010_=(int(*)(strp_nc_0x0010 *const restrict,STRP_NC_0X0010 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_nc_0x0010 *const restrict,STRP_NC_0X0040 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0100_=(int(*)(strp_nc_0x0010 *const restrict,STRP_NC_0X0100 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0400_=(int(*)(strp_nc_0x0010 *const restrict,STRP_NC_0X0400 *const restrict))(StrP_NC_Cast_0x0010_)
+			},
+			.x0040=
+			{
+				.x0010_=(int(*)(strp_nc_0x0040 *const restrict,STRP_NC_0X0010 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_nc_0x0040 *const restrict,STRP_NC_0X0040 *const restrict))(StrP_NC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_nc_0x0040 *const restrict,STRP_NC_0X0100 *const restrict))(StrP_NC_Cast_0x0040_),
+				.x0400_=(int(*)(strp_nc_0x0040 *const restrict,STRP_NC_0X0400 *const restrict))(StrP_NC_Cast_0x0040_)
+			},
+			.x0100=
+			{
+				.x0010_=(int(*)(strp_nc_0x0100 *const restrict,STRP_NC_0X0010 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_nc_0x0100 *const restrict,STRP_NC_0X0040 *const restrict))(StrP_NC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_nc_0x0100 *const restrict,STRP_NC_0X0100 *const restrict))(StrP_NC_Cast_0x0100_),
+				.x0400_=(int(*)(strp_nc_0x0100 *const restrict,STRP_NC_0X0400 *const restrict))(StrP_NC_Cast_0x0100_)
+			},
+			.x0400=
+			{
+				.x0010_=(int(*)(strp_nc_0x0400 *const restrict,STRP_NC_0X0010 *const restrict))(StrP_NC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_nc_0x0400 *const restrict,STRP_NC_0X0040 *const restrict))(StrP_NC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_nc_0x0400 *const restrict,STRP_NC_0X0100 *const restrict))(StrP_NC_Cast_0x0100_),
+				.x0400_=(int(*)(strp_nc_0x0400 *const restrict,STRP_NC_0X0400 *const restrict))(StrP_NC_Cast_0x0400_)
+			}
+		}
+	},
+	.WC=
+	{
+		.Length=
+		{
+			.x0010_=StrP_WC_Length_0x0010_,
+			.x0040_=StrP_WC_Length_0x0040_,
+			.x0100_=StrP_WC_Length_0x0100_,
+			.x0400_=StrP_WC_Length_0x0400_
+		},
+		.Find=
+		{
+			.Char=
+			{
+				.x0010_=StrP_WC_Find_0x0010_,
+				.x0040_=StrP_WC_Find_0x0040_,
+				.x0100_=StrP_WC_Find_0x0100_,
+				.x0400_=StrP_WC_Find_0x0400_
+			},
+			.String=
+			{
+				.x0010_=StrP_WC_Search_0x0010_,
+				.x0040_=StrP_WC_Search_0x0040_,
+				.x0100_=StrP_WC_Search_0x0100_,
+				.x0400_=StrP_WC_Search_0x0400_
+			}
+		},
+		.Reset=
+		{
+			.x0010_=StrP_WC_Reset_0x0010_,
+			.x0040_=StrP_WC_Reset_0x0040_,
+			.x0100_=StrP_WC_Reset_0x0100_,
+			.x0400_=StrP_WC_Reset_0x0400_
+		},
+		.Copy=
+		{
+			.x0010_=StrP_WC_Copy_0x0010_,
+			.x0040_=StrP_WC_Copy_0x0040_,
+			.x0100_=StrP_WC_Copy_0x0100_,
+			.x0400_=StrP_WC_Copy_0x0400_
+		},
+		.Conc=
+		{
+			.x0010_=StrP_WC_Conc_0x0010_,
+			.x0040_=StrP_WC_Conc_0x0040_,
+			.x0100_=StrP_WC_Conc_0x0100_,
+			.x0400_=StrP_WC_Conc_0x0400_
+		},
+		.Crop=
+		{
+			.x0010_=StrP_WC_Crop_0x0010_,
+			.x0040_=StrP_WC_Crop_0x0040_,
+			.x0100_=StrP_WC_Crop_0x0100_,
+			.x0400_=StrP_WC_Crop_0x0400_
+		},
+		.Comp=
+		{
+			.x0010_=StrP_WC_Comp_0x0010_,
+			.x0040_=StrP_WC_Comp_0x0040_,
+			.x0100_=StrP_WC_Comp_0x0100_,
+			.x0400_=StrP_WC_Comp_0x0400_
+		},
+		.Cast=
+		{
+			.x0010=
+			{
+				.x0010_=(int(*)(strp_wc_0x0010 *const restrict,STRP_WC_0X0010 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_wc_0x0010 *const restrict,STRP_WC_0X0040 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0100_=(int(*)(strp_wc_0x0010 *const restrict,STRP_WC_0X0100 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0400_=(int(*)(strp_wc_0x0010 *const restrict,STRP_WC_0X0400 *const restrict))(StrP_WC_Cast_0x0010_)
+			},
+			.x0040=
+			{
+				.x0010_=(int(*)(strp_wc_0x0040 *const restrict,STRP_WC_0X0010 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_wc_0x0040 *const restrict,STRP_WC_0X0040 *const restrict))(StrP_WC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_wc_0x0040 *const restrict,STRP_WC_0X0100 *const restrict))(StrP_WC_Cast_0x0040_),
+				.x0400_=(int(*)(strp_wc_0x0040 *const restrict,STRP_WC_0X0400 *const restrict))(StrP_WC_Cast_0x0040_)
+			},
+			.x0100=
+			{
+				.x0010_=(int(*)(strp_wc_0x0100 *const restrict,STRP_WC_0X0010 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_wc_0x0100 *const restrict,STRP_WC_0X0040 *const restrict))(StrP_WC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_wc_0x0100 *const restrict,STRP_WC_0X0100 *const restrict))(StrP_WC_Cast_0x0100_),
+				.x0400_=(int(*)(strp_wc_0x0100 *const restrict,STRP_WC_0X0400 *const restrict))(StrP_WC_Cast_0x0100_)
+			},
+			.x0400=
+			{
+				.x0010_=(int(*)(strp_wc_0x0400 *const restrict,STRP_WC_0X0010 *const restrict))(StrP_WC_Cast_0x0010_),
+				.x0040_=(int(*)(strp_wc_0x0400 *const restrict,STRP_WC_0X0040 *const restrict))(StrP_WC_Cast_0x0040_),
+				.x0100_=(int(*)(strp_wc_0x0400 *const restrict,STRP_WC_0X0100 *const restrict))(StrP_WC_Cast_0x0100_),
+				.x0400_=(int(*)(strp_wc_0x0400 *const restrict,STRP_WC_0X0400 *const restrict))(StrP_WC_Cast_0x0400_)
+			}
+		}
+	},
+	.Errno=
+	{
+		.Zero=0,
+		.Dom=EDOM,
+		.IlSeq=EILSEQ,
+		.Range=ERANGE
+	},
+	.Version=_StringVersion
+};
+STRPACK *StrP_(void) { return &StrP; }
+#endif
+
+#else
+#error The compiler does not support the C Library Extension 1.
+#endif
+
+#endif
+
+#else
+#error The compiler does not specify the C Standard version number.
+#endif
+
+#else
+#error The compiler does not conform to the C Standard.
+#endif
+
+#else
+#error The compiler does not conform to the C Standard.
+#endif
