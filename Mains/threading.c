b72e96add0dcbed2b5665f649292a9ad67b25ebc
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2019-11-22 15:11:17 +0900

    Add files via upload

--- Mains/threading.c
+++ Mains/threading.c
@@ -2,62 +2,36 @@
 #define restrict __restrict
 #endif
 
-#include <stdbool.h>
-#include <stdio.h>
 #include "thrpure.h"
+#include <stdio.h>
 
-struct _my_task
-{
-	const char *Msg;
-	int Period;
-	int Repeat;
-};
-typedef struct _my_task my_task;
-typedef const struct _my_task MY_TASK;
+typedef const struct { const char *const Msg;const int Period;const int Repeat; }MY_TASK;
 
 static _Bool _My_Process_(const void *const Arg);
 
-#define _My_Do_(Flag,Oper) do{if(Flag){return(Flag);}else{(Flag)=(Oper);}}while(0)
-
 int main(void)
 {
-	const char NameA[]="[Fine]";
-	const char NameB[]="[thank you]";
-	const size_t QueueBytes=1024;
+#define _My_Do_(Flag,Oper) if(Flag)return(Flag);else do{(Flag)=(Oper);}while(0)
 	THRPACK T=*ThrP_();
+	const char NameA[]="[Fine]",NameB[]="[thank you]";
+	const size_t QueueBytes=1024;
 	thrp_qu *Qu[2]={NULL,NULL};
-	my_task _TaskHolder,*const TaskHolder=&_TaskHolder;
 	int Flag=T.Flag.Success;
 	
 	if(Flag)
-	{
 		puts("ThrP.Flag.Success != 0");
-
-		return Flag;
-	}
+	else;
 
 	_My_Do_(Flag,T.Qu.Create_(Qu+0,QueueBytes));
 	_My_Do_(Flag,T.Qu.Create_(Qu+1,QueueBytes));
 
 	_My_Do_(Flag,T.Qu.Push_(Qu+0,(thrp_p_)(T.Task.Print_),NameA,sizeof(NameA)));
 	_My_Do_(Flag,T.Qu.Push_(Qu+1,(thrp_p_)(T.Task.Print_),NameB,sizeof(NameB)));
-	_My_Do_(Flag,T.Task.Yield_(NULL));
-
-	TaskHolder->Repeat=3;
-	TaskHolder->Period=250;
-	TaskHolder->Msg="Hi, there!";
-	_My_Do_(Flag,T.Qu.Push_(Qu+0,_My_Process_,TaskHolder,sizeof(my_task)));
-
-	TaskHolder->Msg="Who are you?";
-	_My_Do_(Flag,T.Qu.Push_(Qu+1,_My_Process_,TaskHolder,sizeof(my_task)));
-
-	TaskHolder->Repeat=5;
-	TaskHolder->Period=500;
-	TaskHolder->Msg="I'm Fine, thank you.";
-	_My_Do_(Flag,T.Qu.Push_(Qu+0,_My_Process_,TaskHolder,sizeof(my_task)));
-
-	TaskHolder->Msg="Nice to meet you, Fine.";
-	_My_Do_(Flag,T.Qu.Push_(Qu+1,_My_Process_,TaskHolder,sizeof(my_task)));
+	
+	_My_Do_(Flag,T.Qu.Push_(Qu+0,_My_Process_,&(MY_TASK){.Repeat=3,.Period=250,.Msg="Hi, there!"},sizeof(MY_TASK)));
+	_My_Do_(Flag,T.Qu.Push_(Qu+1,_My_Process_,&(MY_TASK){.Repeat=3,.Period=250,.Msg="Who are you?"},sizeof(MY_TASK)));
+	_My_Do_(Flag,T.Qu.Push_(Qu+0,_My_Process_,&(MY_TASK){.Repeat=5,.Period=500,.Msg="I'm Fine, thank you."},sizeof(MY_TASK)));
+	_My_Do_(Flag,T.Qu.Push_(Qu+1,_My_Process_,&(MY_TASK){.Repeat=5,.Period=500,.Msg="Nice to meet you, Fine."},sizeof(MY_TASK)));
 
 	_My_Do_(Flag,T.Qu.Wait_(Qu+0));
 	_My_Do_(Flag,T.Qu.Wait_(Qu+1));
@@ -66,6 +40,7 @@ int main(void)
 	_My_Do_(Flag,T.Qu.Delete_(Qu+0));
 
 	return Flag;
+#undef _My_Do_
 }
 
 static _Bool _My_Process_(const void *const Arg)
