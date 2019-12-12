404c2993bb9179b350c669c9e50325c861866ca6
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2019-12-12 18:10:45 +0900

    Add files via upload

--- Mains/threading.c
+++ Mains/threading.c
@@ -1,62 +1,110 @@
-﻿#ifdef _MSC_BUILD
-#define _Alignas(Size)
+﻿#ifdef _MSC_BUILD//workaround for MSVC issues
 #define restrict __restrict
-#pragma warning(disable:4201)
+#define _Alignas(Size)
 static_assert(sizeof(void*)==sizeof(void(*)(void)),"");
+#define _USE_32BIT_TIME_T//to match the 'time_t' between build and usage
+#pragma warning(disable:4201 4204)
 #endif
 
-#define _PURES_DLL_IMPORT_ __declspec(dllimport)
+#define _PURES_DLL_IMPORT_ __declspec(dllimport)//only for DLL usage
+#define _THRP_MACRO_DEFINE_
+#define _TIMP_MACRO_DEFINE_
 #include <thrpure.h>
+#include <timpure.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef const struct { const char *const Msg;const int Period,Repeat; }MY_TASK;
-static _Bool _My_Process_(const void *const Arg);
+typedef const struct { const struct timespec Time; }MY_TIME;//wrapper for 'struct timespec'
+typedef const struct { const int Period,Repeat;const char Msg[32]; }MY_TASK;//task argument holder
 
+//ABI test but not sufficient
+static void _My_Test_(void);
+//function for event example
+static _Bool _My_Event_(const void *const);
+//function for task example
+static _Bool _My_Process_(const void *const);
+
+//main logic
 int main(void)
-#define _My_Do_(Flag,Oper) if(Flag)return(Flag);else do{(Flag)=(Oper);}while(0)
+#define _My_Do_(Flag,Oper) do{if((Flag)==(ThrP.Flag->Success))(Flag)=(Oper);else return(Flag);}while(0)
 {
-	const char NameA[]="[Fine]",NameB[]="[thank you]";
-	const size_t QueueBytes=1024;
-	thrp_qu *Qu[2]={NULL,NULL};
-	int Flag=ThrP.Flag->Success;
+	_My_Test_();
 
-	if(Flag)
-		puts("ThrP.Flag->Success != 0");
-	else;
-
-	_My_Do_(Flag,ThrP.Qu.Create_(Qu+0,QueueBytes));
-	_My_Do_(Flag,ThrP.Qu.Create_(Qu+1,QueueBytes));
-
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+0,ThrP.Task.Print_,NameA,sizeof(NameA)));
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+1,ThrP.Task.Print_,NameB,sizeof(NameB)));
+	const size_t QueueSpace=1024;//internal queue space - 1KB
+	thrp_qu *Qu[2]={NULL,NULL};//queue object table
+	int Flag=ThrP.Flag->Success;//thread flag
 	
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+0,_My_Process_,&(MY_TASK){.Repeat=3,.Period=250,.Msg="Hi, there!"},sizeof(MY_TASK)));
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+1,_My_Process_,&(MY_TASK){.Repeat=3,.Period=250,.Msg="Who are you?"},sizeof(MY_TASK)));
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+0,_My_Process_,&(MY_TASK){.Repeat=5,.Period=500,.Msg="I'm Fine, thank you."},sizeof(MY_TASK)));
-	_My_Do_(Flag,ThrP.Qu.Push_(Qu+1,_My_Process_,&(MY_TASK){.Repeat=5,.Period=500,.Msg="Nice to meet you, Fine."},sizeof(MY_TASK)));
+	_My_Do_(Flag,ThrP_Event_Invoke_(_My_Event_,MY_TIME,.Time=TimP.Spec.Now_()));//send an event arg
+	
+	_My_Do_(Flag,ThrP.Qu.Create_(Qu+0,QueueSpace));//create queue 0
+	_My_Do_(Flag,ThrP.Qu.Create_(Qu+1,QueueSpace));//create queue 1
+	
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+0,ThrP.Task.Sleep_,const int,+10));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+1,ThrP.Task.Sleep_,const int,+10));
 
-	_My_Do_(Flag,ThrP.Qu.Wait_(Qu+0));
-	_My_Do_(Flag,ThrP.Qu.Wait_(Qu+1));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+0,ThrP.Task.Print_,const char[8],"[Fine]"));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+1,ThrP.Task.Print_,const char[12],"[thank you]"));
 
-	_My_Do_(Flag,ThrP.Qu.Delete_(Qu+1));
-	_My_Do_(Flag,ThrP.Qu.Delete_(Qu+0));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+0,_My_Process_,MY_TASK,.Repeat=+3,.Period=+250,.Msg="Hi, there!"));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+1,_My_Process_,MY_TASK,.Repeat=+3,.Period=+250,.Msg="Who are you?"));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+0,_My_Process_,MY_TASK,.Repeat=+5,.Period=+500,.Msg="I'm Fine, thank you."));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+1,_My_Process_,MY_TASK,.Repeat=+5,.Period=+500,.Msg="Nice to meet you, Fine."));
+	
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+0,ThrP.Task.Sleep_,const int,+750));
+	_My_Do_(Flag,ThrP_Qu_Push_(Qu+1,ThrP.Task.Sleep_,const int,+750));
+	
+	_My_Do_(Flag,ThrP.Qu.Wait_(Qu+0));//synchronize queue 0
+	_My_Do_(Flag,ThrP.Qu.Wait_(Qu+1));//synchronize queue 1
 
+	_My_Do_(Flag,ThrP.Qu.Delete_(Qu+0));//delete queue 0
+	_My_Do_(Flag,ThrP.Qu.Delete_(Qu+1));//delete queue 1
+	
 	return Flag;
 }
 #undef _My_Do_
 
-static _Bool _My_Process_(const void *const Arg)
+static void _My_Test_(void)
 {
-	MY_TASK *const Task=Arg;
+	if((TimP.Size->Double)==sizeof(double))//'double' mismatch
+		if((TimP.Size->TimeSpec)==sizeof(struct timespec))//'struct timespec' mismatch
+		{
+			const double Number=+2.5;//any number which would not truncate during cast
 
-	for(int Count=0;Count<Task->Repeat;Count++)
-		if(ThrP.Task.Sleep_(&(Task->Period))==ThrP.Signal->Continue)
-			if(printf("[%3d ms] [%d/%d] %s\r\n",Task->Period,Count,Task->Repeat,Task->Msg)<0)
-				return ThrP.Signal->Break;
+			if(TimP.Spec.Get_(TimP.Spec.Set_(Number))==Number)
+				return;
+			else;
+		}
+		else;
+	else;
+
+	puts("Library build and usage is not matched!");
+	ThrP.Task.Sleep_(&(const int) { +999 });
+	exit(EXIT_FAILURE);
+}
+static _Bool _My_Event_(const void *const Arg)//receive an event
+{
+	const struct timespec Old=*((const struct timespec*)(Arg));//send time
+	const struct timespec New=TimP.Spec.Now_();//receive time
+	const struct timespec Diff=TimP.Spec.Sub_(New,Old);//time difference
+
+	if(printf("Sending to receiving : about %+.3Es seconds have past.\r\n",TimP.Spec.Get_(Diff))<0)
+		return ThrP.Signal->Break;
+	else
+		return ThrP.Signal->Continue;
+}
+static _Bool _My_Process_(const void *const Arg)//receive a task
+{
+	MY_TASK Task=*((MY_TASK*)(Arg));//task argument holder
+	const struct _thrpack_signal Sign=*(ThrP.Signal);//signal table
+
+	for(int Count=0;Count<Task.Repeat;Count++)
+		if(ThrP.Task.Sleep_(&(Task.Period))==Sign.Continue)
+			if(printf("[%3d ms] [%d/%d] %s\r\n",Task.Period,Count,Task.Repeat,Task.Msg)<0)
+				return Sign.Break;
 			else
 				continue;
 		else
-			return ThrP.Signal->Break;
+			return Sign.Break;
 
-	return ThrP.Signal->Continue;
+	return Sign.Continue;
 }
